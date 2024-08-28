ec8a650e6d30cefb72da6b10a9b0ce68e1cc99b8
akshaybusa <146056552+Akbusa009@users.noreply.github.com>
2024-08-28 21:32:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,11 @@ connectDB();
 const app = express();
 
 //middelwares
-app.use(cors());
+app.use(cors({
+  origin: "https://ecommerce-app-nine-zeta.vercel.app/", // Change this to your frontend URL
+  methods: ["GET", "POST"],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 
